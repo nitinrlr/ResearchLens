7f60d9dd4e53a1f51f73a31cb9bf4f80b98b3988
nitinrlr <ramaling@sc.edu>
2026-09-01 19:34:34 -0400

    Added protection to dashboard page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,17 @@
 import Sidebar from "@/components/sidebar";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className="flex min-h-screen bg-black text-white">
       <Sidebar />
